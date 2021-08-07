a9abc0b26aab3974d5687adee03673798c66dae7
Noah Witt <noah@noahw.dev>
2021-08-07 17:07:18 -0500

    basic registers

--- registers.cpp
+++ registers.cpp
@@ -7,3 +7,122 @@
  * 
  * 
  */
+
+#include "registers.hpp"
+#include "memory.hpp"
+#include <stdlib.h>
+#include <unordered_map>
+# define REGISTER_WIDTH_BYTES 8
+# define REGISTERS_END 31
+
+Register::Register() {
+    this->value = (unsigned long *)calloc(REGISTER_WIDTH_BYTES, sizeof(unsigned char));
+}
+
+Register::~Register() {
+    free(this->value);
+}
+
+readResult<unsigned char> Register::readByte() {
+    readResult<unsigned char> result;
+    try {
+        result.payload = (unsigned char)*(this->value);
+        result.valid = true;
+        return result;
+    } catch(int e) {
+        result.valid = false;
+        result.payload = 0x0;
+    }
+    return result;
+}
+
+readResult<unsigned short> Register::readShort() {
+    readResult<unsigned short> result;
+    try {
+        result.payload = (unsigned short)*(this->value);
+        result.valid = true;
+        return result;
+    } catch(int e) {
+        result.valid = false;
+        result.payload = 0x0;
+    }
+    return result;
+}
+
+readResult<unsigned int> Register::read() {
+    readResult<unsigned int> result;
+    try {
+        result.payload = (unsigned int)*(this->value);
+        result.valid = true;
+        return result;
+    } catch(int e) {
+        result.valid = false;
+        result.payload = 0x0;
+    }
+    return result;
+}
+
+readResult<unsigned long> Register::readLong() {
+    readResult<unsigned long> result;
+    try {
+        result.payload = (unsigned long)*(this->value);
+        result.valid = true;
+        return result;
+    } catch(int e) {
+        result.valid = false;
+        result.payload = 0x0;
+    }
+    return result;
+}
+
+bool Register::writeByte(unsigned char in) {
+    return this->writeLong((unsigned long) in);
+}
+
+bool Register::writeShort(unsigned short in) {
+    return this->writeLong((unsigned long) in);
+}
+
+bool Register::write(unsigned int in) {
+    return this->writeLong((unsigned int) in);
+}
+
+bool Register::writeLong(unsigned long in) {
+    *this->value = in;
+    return true;
+}
+
+
+zeroRegister::zeroRegister() {
+    this->value = (unsigned long *)calloc(REGISTER_WIDTH_BYTES, sizeof(unsigned char));
+    *this->value =   0x0;
+}
+
+
+bool zeroRegister::writeLong(unsigned long in) {
+    return false;
+}
+
+
+Registers::Registers() {
+    this->registers = std::unordered_map<unsigned int, Register>();
+    this->registers.insert(std::pair<unsigned int, Register>(0, zeroRegister()));
+    for(unsigned int i=1; i<=REGISTERS_END; i++) {
+        //create registers for [1, REGISTERS_END]
+        this->registers.insert(std::pair<unsigned int, Register>(i, Register()));
+    }
+    // TODO add the FP registers just store with offset
+}
+
+Registers::~Registers() {
+    std::unordered_map<unsigned int, Register>::iterator it = this->registers.begin();
+    while(it != this->registers.end()) {
+        it->second.~Register();
+    }
+    this->registers.~unordered_map();
+}
+
+Register Registers::getRegister(unsigned int x) {
+    std::unordered_map<unsigned int, Register>::iterator items = this->registers.find(x);
+    return items->second;
+}
--- registers.hpp
+++ registers.hpp
@@ -1,4 +1,39 @@
+#pragma once
+
+#include "memory.hpp"
+#include <unordered_map>
+
+/**
+ * @brief a basic register
+ * 
+ */
+class Register {
+    protected:
+        unsigned long *value;
+    public:
+        Register();
+        ~Register();
+        readResult<unsigned char> readByte();
+        readResult<unsigned short> readShort();
+        readResult<unsigned int> read();
+        readResult<unsigned long> readLong();
+        bool writeByte(unsigned char);
+        bool writeShort(unsigned short);
+        bool write(unsigned int);
+        bool writeLong(unsigned long);
+};
+
+class zeroRegister: public Register {
+    public:
+        bool writeLong(unsigned long);
+};
 
 class Registers {
-    // TODO  
+    // TODO  store all the registers and 
+    protected:
+        std::unordered_map<unsigned int, Register> registers;
+    public:
+        Registers();
+        ~Registers();
+        Register getRegister(unsigned int);
 };
